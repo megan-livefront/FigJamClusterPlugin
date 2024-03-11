d94b8a915f140f7e29f321bd695ac1fff28a846f
meganpekarek <megan@livefront.com>
2024-03-11 09:40:08 -0500

    auto focus in put and cluster better

--- code.ts
+++ code.ts
@@ -14,8 +14,47 @@ figma.ui.onmessage = (msg: { type: string; clusterString: string }) => {
       }
     });
 
-    figma.currentPage.selection = nodesToCluster;
+    const currentViewPort = figma.currentPage.selection[0].absoluteBoundingBox;
+    if (currentViewPort) {
+      nodesToCluster.forEach((node, index) => {
+        const positiveIndex = index + 1;
+        node.x = getNodeX(currentViewPort.x, node, positiveIndex);
+        node.y = getNodeY(currentViewPort.y, node, positiveIndex);
+      });
+
+      figma.currentPage.selection = nodesToCluster;
+    }
   }
 
   figma.closePlugin();
 };
+
+function getNodeX(
+  currentX: number,
+  node: ShapeWithTextNode | StickyNode,
+  index: number
+) {
+  const nodeWidth = node.width + 25; // Add spacing between each node
+  if (index > 3) {
+    if (index % 3 === 0) {
+      return currentX - nodeWidth * 3;
+    } else {
+      return currentX - nodeWidth * (index % 3);
+    }
+  } else {
+    return currentX - nodeWidth * index;
+  }
+}
+
+function getNodeY(
+  currentY: number,
+  node: ShapeWithTextNode | StickyNode,
+  index: number
+) {
+  const nodeHeight = node.height + 25; // Add spacing between each node
+  if (index > 3) {
+    return currentY + Math.ceil(index / 3) * nodeHeight;
+  } else {
+    return currentY + nodeHeight;
+  }
+}
